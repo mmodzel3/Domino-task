62a0de77028f139f8c48279a26934089834bcc76
mmodzel3 <45737456+mmodzel3@users.noreply.github.com>
2021-03-21 12:21:31 +0100

    Added argument parsing.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import argparse
+
 
 def main():
-    print('Task - Domino')
+    parser = argparse.ArgumentParser(description='Simulates domino arrangement.')
+    parser.add_argument('domino', type=str, help='domino arrangement to simulate, ex. //||\\//\\')
+
+    parser.add_argument('iterations', type=int, help='iterations of domino game')
+
+    parser.add_argument('--forward', dest='forward', action='store_true')
+    parser.add_argument('--backward', dest='forward', action='store_false')
+    parser.set_defaults(forward=True)
+
+    args = parser.parse_args()
+
+    if args.iterations < 0:
+        print('Iterations argument cannot be negative.')
+        return
+
 
 
 if __name__ == "__main__":
